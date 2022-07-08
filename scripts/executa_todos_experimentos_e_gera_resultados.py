4232574e4a1d5100b398f93b860c412298b8cf58
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-07 23:43:47 -0300

    adicionado contagem de tempo para execucao dos testes

--- scripts/executa_todos_experimentos_e_gera_resultados.py
+++ scripts/executa_todos_experimentos_e_gera_resultados.py
@@ -9,15 +9,19 @@ from pathlib import Path
 try:
     import paramiko
     import argparse
+    import timeit
 
 except:
     os.system('pip install argparse')
     os.system('pip install paramiko')
 
+#implementacao -----------------------------------------------------------------------------------------------
+tempo_inicial_geral = timeit.default_timer()
+
+
 #variaveis globais ----------------------------
 usuario= 'igorubuntu'
 #usuario= 'igorcapeletti'
-
 tipo_programa_ebpf= 'for'
 secao_programa_ebpf= 'xdp_test'
 modo_execucao_programa_ebpf= 'normal'
@@ -39,6 +43,7 @@ cont_intervalo= 500
 lista_experimentos= list(range(cont_inicial, cont_final, cont_intervalo))
 for experimento in lista_experimentos:
   #1)-Gera programa eBPF ---------------------------------------------------------------------------------------------
+  tempo_inicial = timeit.default_timer()
   if(tipo_programa_ebpf == 'for'):
     nome_arq_algoritmo= f'for_{cont_inicial}_a_{experimento}.c'
     pasta_resultado= nome_arq_algoritmo
@@ -99,8 +104,8 @@ for experimento in lista_experimentos:
     timeout_gerador= 120
     lista_modos_exec_xdp= ['xdpgeneric','xdpdrv','xdpoffload']
     lista_tam_pacotes_gerar= ['64','128','256','512','1024','1500']
-    lista_variacao_IPs= ['0.0.0.0','0.0.0.255','0.0.255.255','0.255.255.255','255.255.255.255']
-    lista_variacao_MACs= '00:00:00:00:00:00'  
+    lista_variacao_ips= ['0.0.0.0','0.0.0.255','0.0.255.255','0.255.255.255','255.255.255.255']
+    lista_variacao_macs= '00:00:00:00:00:00'  
 
     #4)-Compila programa e carrega para a interface de rede nos modos xdp que escolher ------------------------------------
     for modo_xdp in lista_modos_exec_xdp:   #vai fazer o experimento para cada um dos Hooks do XDP
@@ -109,16 +114,22 @@ for experimento in lista_experimentos:
       #5)-Faz acesso ssh com maquina geradora de trafego e cria trafego para os tamanhos de pacotes escolhidos ------------
       stdin,stdout,stderr= ssh_client.exec_command(f'cd /home/{usuario_ssh}/github/tcc_eBPF_XDP/gerador_trafego')
       for tam_packet in lista_tam_pacotes_gerar:  #vai fazer o experimento para cada um dos tamanhos de pacotes
-        for var_ip in lista_variacao_IPs:           #vai fazer o experimento para cada variacao de IPs
+        for var_ip in lista_variacao_ips:           #vai fazer o experimento para cada variacao de IPs
           #executa o gerador e coloeta os dados tx/rx dos espeximento e salva dentro de uma pasta num arquivo especifico
           stdin,stdout,stderr= ssh_client.exec_command(f'bash setupNetGen.sh {tam_packet} {modo_xdp} {var_ip} 00:00:00:00:00:00 {timeout_gerador} {pasta_resultado}')
           
           #6)-Carrega os resultados do experimento e adiciona as medias em novo arquivo para gerar graficos ---------------
-          arq_save_resultado=f"/home/{usuario_ssh}/github/tcc_eBPF_XDP/resultados/{pasta_resultado}/res_pkt{tam_packet}_ebpf_{modo_xdp}+{modo_execucao_programa_ebpf}_varIP_{var_ip}_varMAC_{lista_variacao_MACs}.txt"
+          arq_save_resultado=f"/home/{usuario_ssh}/github/tcc_eBPF_XDP/resultados/{pasta_resultado}/res_pkt{tam_packet}_ebpf_{modo_xdp}+{modo_execucao_programa_ebpf}_varIP_{var_ip}_varMAC_{lista_variacao_macs}.txt"
           stdin,stdout,stderr= ssh_client.exec_command(f'cd /home/{usuario_ssh}/github/tcc_eBPF_XDP/scripts/python')
           stdin,stdout,stderr= ssh_client.exec_command(f'python3 gera_csv_resultado.py --arquivo {arq_save_resultado}')
 
   elif(modo_execucao_programa_ebpf == 'af_xdp'):   #modo exec eBPF com AF_XDP
     print('Modo de execucao do programa ebpf(af_xdp), ainda nao desenvolvido!')
 
+  tempo_final = timeit.default_timer()
+  print (f'\nDuracao dos experimentos {nome_arq_algoritmo} nos hooks XDP {lista_modos_exec_xdp} com tamanho de pacotes {lista_tam_pacotes_gerar} e variacao de IP {lista_variacao_ips} : {(tempo_final-tempo_inicial)} segundos')
+
 ssh_client.close()
+
+tempo_final_geral = timeit.default_timer()
+print (f'\nDuracao total das execuções: {(tempo_final_geral-tempo_inicial_geral)} segundos')
