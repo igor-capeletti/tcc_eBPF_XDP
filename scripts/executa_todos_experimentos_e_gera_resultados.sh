17bff55f29fe254a69d0be5d0febef34496870c6
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-11 15:43:30 -0300

    t

--- scripts/executa_todos_experimentos_e_gera_resultados.sh
+++ scripts/executa_todos_experimentos_e_gera_resultados.sh
@@ -138,14 +138,14 @@ for it_combined in "8"; do
             for it_var_ip in "0.0.0.255"; do
               #faz acesso ssh com maquina geradora de trafego e chama shell script que ativa o gerador para gerar trafego
               echo "Gerador enviando e recenbendo tráfego..."
-              ssh $ssh_usuario_gerador@$ssh_ip_gerador echo $PASS | sudo -S bash $ssh_local_gerador/setupNetGen.sh $it_tam_packet $it_modo_xdp $it_var_ip $it_combined $timeout_gerador $pasta_resultado
+              ssh $ssh_usuario_gerador@$ssh_ip_gerador 'echo $PASS | sudo -S bash $ssh_local_gerador/setupNetGen.sh $it_tam_packet $it_modo_xdp $it_var_ip $it_combined $timeout_gerador $pasta_resultado'
               
               
               sleep "130"
     
               #coleta a media dos resultados obtidos e salva em um arquivo geral da pasta
               arq_save_resultado="$ssh_local_resultados/$pasta_resultado/res_combined_$combined+algoritmo_$pasta_resultado+pkt_$tam_packet+ebpf_$modo_xdp+varIP_$var_ip+timeout_$timeout.txt"
-              ssh $ssh_usuario_gerador@$ssh_ip_gerador python3 $ssh_local_scripts_python/gera_csv_resultado.py --arquivo $arq_save_resultado
+              ssh $ssh_usuario_gerador@$ssh_ip_gerador 'python3 $ssh_local_scripts_python/gera_csv_resultado.py --arquivo $arq_save_resultado'
 
               #prints
               echo "Experimento $cont/$((3*21*2*6)): ----------------------------------"
