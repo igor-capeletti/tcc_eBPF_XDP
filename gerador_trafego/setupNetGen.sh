5982817539c49ae9bb27a67c03937d2369c7d942
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-11 16:13:33 -0300

    teste

--- gerador_trafego/setupNetGen.sh
+++ gerador_trafego/setupNetGen.sh
@@ -18,20 +18,18 @@ cd /opt/MoonGen
 
 #load driver
 modprobe uio
-insmod libmoon/deps/dpdk/x86_64-native-linux-gcc/kmod/igb_uio.ko
+insmod /opt/MoonGen/libmoon/deps/dpdk/x86_64-native-linux-gcc/kmod/igb_uio.ko
 
 #attach driver to network interfaface
-python /opt/MoonGen/libmoon/deps/dpdk/usertools/dpdk-devbind.py -u 0000:23:00.1
+python libmoon/deps/dpdk/usertools/dpdk-devbind.py -u 0000:23:00.1
 
-python /opt/MoonGen/libmoon/deps/dpdk/usertools/dpdk-devbind.py --bind=igb_uio 0000:21:00.0
+python libmoon/deps/dpdk/usertools/dpdk-devbind.py --bind=igb_uio 0000:21:00.0
 
-python /opt/MoonGen/libmoon/deps/dpdk/usertools/dpdk-devbind.py --status
+python libmoon/deps/dpdk/usertools/dpdk-devbind.py --status
 
 
 #/opt/MoonGen/build/MoonGen ./examples/netronome-packetgen/packetgen.lua -tx 1 -rx 0 --dst-ip 200.10.12.3 --dst-ip-vary 0.0.0.0 --dst-mac 00:15:4d:13:63:3c --dst-mac-vary 00:00:00:00:00:00 --src-mac 90:E2:BA:4E:8A:A4 --src-mac-vary 00:00:00:00:00:00 --src-ip 11.11.11.10 --src-ip-vary 0.0.0.0
 
-
+/opt/MoonGen/build/MoonGen ./examples/netronome-packetgen/packetgen.lua -tx 0 -rx 0 --dst-ip 10.10.10.10 --dst-ip-vary $var_dst_IP --dst-mac 00:15:4d:13:5b:d3 --dst-mac-vary 00:00:00:00:00:00 --src-mac 90:E2:BA:4E:8A:A4 --src-mac-vary 00:00:00:00:00:00 --src-ip 10.10.10.1 --src-ip-vary 0.0.0.0 --pkt-size $packet_size --timeout $timeout
 #execucao do gerador de trafego a partir dos parametros passados
-/opt/MoonGen/build/MoonGen ./examples/netronome-packetgen/packetgen.lua -tx 0 -rx 0 --dst-ip 10.10.10.10 --dst-ip-vary $var_dst_IP --dst-mac 00:15:4d:13:5b:d3 --dst-mac-vary 00:00:00:00:00:00 --src-mac 90:E2:BA:4E:8A:A4 --src-mac-vary 00:00:00:00:00:00 --src-ip 10.10.10.1 --src-ip-vary 0.0.0.0 --pkt-size $packet_size --timeout $timeout --file-prefix $arq_save 
-
-echo "Executou gerador!"
+#/opt/MoonGen/build/MoonGen ./examples/netronome-packetgen/packetgen.lua -tx 0 -rx 0 --dst-ip 10.10.10.10 --dst-ip-vary $var_dst_IP --dst-mac 00:15:4d:13:5b:d3 --dst-mac-vary 00:00:00:00:00:00 --src-mac 90:E2:BA:4E:8A:A4 --src-mac-vary 00:00:00:00:00:00 --src-ip 10.10.10.1 --src-ip-vary 0.0.0.0 --pkt-size $packet_size --timeout $timeout --file-prefix $arq_save 
