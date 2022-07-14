434ba5bbf1fac40525e002d57a8146016ccb51c1
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-14 13:50:36 -0300

    att gera_grafico

--- scripts/python/gera_graficos_resultados.py
+++ scripts/python/gera_graficos_resultados.py
@@ -15,26 +15,46 @@ raiz= "/home/igorubuntu/Desktop/backup_res"
 
 df = pd.read_csv(f'{raiz}/resultado_geral.csv', sep=',', engine='python')
 
-#gerancao dos graficos ---------------------------------------
+grafico_imprimir= 2
+
+#geracao dos graficos ---------------------------------------
 print(df.columns)
 #Perguntas:
 
-#1ª) Qual a quantidade de pacotes processados quando utilizar laços for de diferentes tamanhos
-# com diferentes tamanhos de pacotes (mantendo o mesmo modo xdp e o mesmo combined)
-combined= 1
-hook_ebpf= "xdpgeneric"
-df_pergunta1= df[df.combined == combined]
-df_pergunta1= df_pergunta1[df_pergunta1.hook_ebpf == hook_ebpf]
-df_pergunta1= df_pergunta1.sort_values(by=['packet_size','algoritmo'], ascending=True)
-df_pergunta1= df_pergunta1[['algoritmo','packet_size','rx_packets','rx_packet_rate_avg','rx_packet_rate']]
+if(grafico_imprimir == 1):
+  plt.rc('figure', figsize=(16, 12))
+  #1ª) Qual a quantidade de pacotes processados quando utilizar laços for de diferentes tamanhos
+  # com diferentes tamanhos de pacotes (mantendo o mesmo modo xdp e o mesmo combined)
+  #it_combined= 1
+  #it_hook_ebpf= "xdpgeneric"
+  #df_pergunta1= df[df.combined == it_combined]
+  #df_pergunta1= df_pergunta1[df_pergunta1.hook_ebpf == it_hook_ebpf]
+  #df_pergunta1= df_pergunta1.sort_values(by=['algoritmo','packet_size'], ascending=True)
+  #df_pergunta1= df_pergunta1[['algoritmo','packet_size','rx_packets','rx_packet_rate_avg','rx_packet_rate']]
+#
+  #ax = sns.barplot(y="rx_packets", x='algoritmo', hue='packet_size', data=df_pergunta1, palette='Paired');
+  #ax.set_title(f'Quantidade de pacotes processados de acordo com o Algoritmo e tamanho de pacotes, com Hook {it_hook_ebpf} e {it_combined} fila tx/rx', fontsize=18, pad=50);
+  #ax.set_ylabel('Quantidade de pacotes')
+  #ax.set_xlabel('Tamanhos do algoritmo for')
+  #ax.legend(title = "Tamanho dos pacotes")
+  #save_image= ax.get_figure()
+  #save_image.savefig(f'{raiz}/pergunta1.pdf')
 
-ax = sns.barplot(y="rx_packets", x='algoritmo', hue='packet_size', data=df_pergunta1, palette='Paired');
-ax.set_title(f'Quantidade de pacotes processados de acordo com o Algoritmo e tamanho de pacotes, com Hook {hook_ebpf} e {combined} fila tx/rx', fontsize=18, pad=50);
-ax.set_ylabel('Quantidade de pacotes')
-ax.set_xlabel('Tamanhos do algoritmo for')
-ax.legend(title = "Tamanho dos pacotes")
+elif(grafico_imprimir == 2):
+  plt.rc('figure', figsize=(16, 12))
+  #2ª) Qual a taxa de vazão de pacotes processados quando utilizar laços for de diferentes tamanhos
+  # com diferentes tamanhos de pacotes (mantendo o mesmo modo xdp e o mesmo combined)
+  it_combined= 1
+  it_hook_ebpf= "xdpgeneric"
+  df_pergunta2= df[df.combined == it_combined]
+  df_pergunta2= df_pergunta2[df_pergunta2.hook_ebpf == it_hook_ebpf]
+  df_pergunta2= df_pergunta2.sort_values(by=['algoritmo','packet_size'], ascending=True)
+  df_pergunta2= df_pergunta2[['algoritmo','packet_size','rx_packets','rx_packet_rate_avg','rx_packet_rate']]
 
-save_image= ax.get_figure()
-save_image.savefig(f'{raiz}/pergunta1.png')
-
-print(df_pergunta1)
+  ax = sns.barplot(y="rx_packet_rate", x='algoritmo', hue='packet_size', data=df_pergunta2, palette='Paired');
+  ax.set_title(f'Taxa de Vazão de pacotes de acordo com o Algoritmo e tamanho de pacotes, com Hook {it_hook_ebpf} e {it_combined} fila tx/rx', fontsize=18, pad=50);
+  ax.set_ylabel("Taxa de vazão")
+  ax.set_xlabel('Tamanhos do algoritmo for')
+  ax.legend(title = "Tamanho dos pacotes")
+  save_image= ax.get_figure()
+  save_image.savefig(f'{raiz}/pergunta2.pdf')
