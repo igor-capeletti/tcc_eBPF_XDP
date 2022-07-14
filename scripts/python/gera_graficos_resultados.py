c97227b68a2365d15023183c0692981fab51c79e
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-13 23:51:17 -0300

    gerando primeiro grafico

--- scripts/python/gera_graficos_resultados.py
+++ scripts/python/gera_graficos_resultados.py
@@ -13,6 +13,27 @@ raiz= "/home/igorubuntu/Desktop/backup_res"
 #raiz= "/home/igorcapeletti/github/tcc_eBPF_XDP/resultados"
 #raiz= "/home/igorubuntu/github/tcc_eBPF_XDP/resultados"
 
-df_caregamento = pd.read_csv(f'{raiz}/resultado_geral.csv', sep=',', engine='python')
+df = pd.read_csv(f'{raiz}/resultado_geral.csv', sep=',', engine='python')
 
-print(df_caregamento)
+#gerancao dos graficos ---------------------------------------
+print(df.columns)
+#Perguntas:
+
+#1ª) Qual a quantidade de pacotes processados quando utilizar laços for de diferentes tamanhos
+# com diferentes tamanhos de pacotes (mantendo o mesmo modo xdp e o mesmo combined)
+combined= 1
+hook_ebpf= "xdpgeneric"
+df_pergunta1= df[df.combined == combined]
+df_pergunta1= df_pergunta1[df_pergunta1.hook_ebpf == hook_ebpf]
+df_pergunta1= df_pergunta1.sort_values(by=['packet_size','algoritmo'], ascending=True)
+df_pergunta1= df_pergunta1[['algoritmo','packet_size','rx_packets','rx_packet_rate_avg','rx_packet_rate']]
+
+ax = sns.barplot(y="rx_packets", x='algoritmo', hue='packet_size', data=df_pergunta1, palette='Paired');
+ax.set_title(f'Quantidade de pacotes processados de acordo com o Algoritmo e tamanho de pacotes, com Hook {hook_ebpf} e {combined} fila tx/rx', fontsize=18, pad=50);
+ax.set_ylabel('Número de pacotes')
+ax.set_xlabel('Tamanho dos pacotes')
+
+save_image= ax.get_figure()
+save_image.savefig(f'{raiz}/pergunta1.png')
+
+print(df_pergunta1)
