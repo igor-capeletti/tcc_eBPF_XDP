13da2b2b0f31718a8081aa895f30fca87adee053
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-07-14 23:04:42 -0300

    gerando csvs para os resultados do gerador e os resultados do PERF

--- scripts/python/gera_csv_resultado.py
+++ scripts/python/gera_csv_resultado.py
@@ -29,20 +29,8 @@ pasta_raiz= ""
 lista_arquivos= []
 lista_pastas= []
 
-
-#end_local_res_gerador= Path.home() / "/home/igorcapeletti/github/tcc_eBPF_XDP/resultados/gerador"
-end_local_res_gerador= Path.home() / "/home/igorubuntu/github/tcc_eBPF_XDP/resultados/gerador"
-
-#end_local_res_perf= Path.home() / "/home/igorcapeletti/github/tcc_eBPF_XDP/resultados/perf"
-end_local_res_perf= Path.home() / "/home/igorubuntu/github/tcc_eBPF_XDP/resultados/perf"
-
-#end_local_res_sar= Path.home() / "/home/igorcapeletti/github/tcc_eBPF_XDP/resultados/sar"
-end_local_res_sar= Path.home() / "/home/igorubuntu/github/tcc_eBPF_XDP/resultados/sar"
-
-
-
 #1) analiza arquivos salvos pelo gerador de trafego ----------------------------------------------------------
-raiz= end_local_res_gerador
+raiz= Path.home() / "/home/igorubuntu/github/tcc_eBPF_XDP/resultados/gerador"
 
 for nomes_pastas in os.walk(raiz):
   lista_pastas.append(nomes_pastas[0])
@@ -85,8 +73,8 @@ for pasta in lista_pastas:
 
 
 #coletar os dados dos arquivos e salvar em um arquivo principal -----------------
-arq_resultado_geral= open(f'{raiz}/resultado_geral_gerador.csv', 'w')
-arq_resultado_geral.write("combined,algoritmo,packet_size,hook_ebpf,var_ip,timeout,rx_packets,rx_packet_rate_avg,rx_packet_rate\n")
+arq_resultado_geral_gerador= open(f'{raiz}/resultado_geral_gerador.csv', 'w')
+arq_resultado_geral_gerador.write("combined,algoritmo,packet_size,hook_ebpf,var_ip,timeout,rx_packets,rx_packet_rate_avg,rx_packet_rate\n")
 
 for pasta in lista_pastas:
   os.chdir(pasta)
@@ -113,27 +101,63 @@ for pasta in lista_pastas:
     rx_packet_rate_avg= df_novo.rxpacketrateavg[0]
     rx_packet_rate= df_novo.rxpacketrate[0]
 
-    arq_resultado_geral.write(f"{combined},{algoritmo},{packet_size},{hook_ebpf},{var_ip},{timeout},{rx_packets},{rx_packet_rate_avg},{rx_packet_rate}\n")
-
-arq_resultado_geral.close()
+    arq_resultado_geral_gerador.write(f"{combined},{algoritmo},{packet_size},{hook_ebpf},{var_ip},{timeout},{rx_packets},{rx_packet_rate_avg},{rx_packet_rate}\n")
+arq_resultado_geral_gerador.close()
 print(f"Todos os resultados do gerador foram salvos no arquivo:\n\t'{raiz}/resultado_geral_gerador.csv'\n")
 
 
 #2) analiza arquivos salvos pelo perf na maquina de execucao dos programas eBPF/XDP ------------------------
 pasta= "/home/igorubuntu/github/tcc_eBPF_XDP/resultados/perf"
+arq_resultado_geral_perf= open(f'{pasta}/resultado_geral_perf.csv', 'w')
+arq_resultado_geral_perf.write("task_clock,context_switches,cpu_migrations,page_faults,cycles,instructions,branches,branch_misses,L1_dcache_loads,L1_dcache_load_misses,LLC_loads,LLC_load_misses,L1_icache_load_misses,dTLB_loads,dTLB_load_misses,iTLB_loads,iTLB_load_misses\n")
+
 os.chdir(pasta)
 for format_file in glob.glob('*.txt'):
   arquivo_txt= open(f'{pasta}/{format_file}', 'r')
   i= 0
   for linha in arquivo_txt:
-    if(i >= 5 and i <= 13):
-      dados_linha= linha.replace(' ','')
-      print(dados_linha)
+    dados_linha= linha.replace(' ','')
+    if(i == 5):
+      task_clock= dados_linha.split("msectask-clock")[0]
+    if(i == 6):
+      context_switches= dados_linha.split("context-switches")[0]
+    if(i == 7):
+      cpu_migrations= dados_linha.split("cpu-migrations")[0]
+    if(i == 8):
+      page_faults= dados_linha.split("page-faults")[0]
+    if(i == 9):
+      cycles= dados_linha.split("cycles")[0]
+    if(i == 10):
+      instructions= dados_linha.split("instructions")[0]
+    if(i == 11):
+      branches= dados_linha.split("branches")[0]
+    if(i == 12):
+      branch_misses= dados_linha.split("branch-misses")[0]
+    if(i == 13):
+      L1_dcache_loads= dados_linha.split("L1-dcache-loads")[0]
+    if(i == 14):
+      L1_dcache_load_misses= dados_linha.split("L1-dcache-load-misses")[0]
+    if(i == 15):
+      LLC_loads= dados_linha.split("LLC-loads")[0]
+    if(i == 16):
+      LLC_load_misses= dados_linha.split("LLC-load-misses")[0]
+
+    if(i == 18):
+      L1_icache_load_misses= dados_linha.split("L1-icache-load-misses")[0]
+    if(i == 19):
+      dTLB_loads= dados_linha.split("dTLB-loads")[0]
+    if(i == 20):
+      dTLB_load_misses= dados_linha.split("dTLB-load-misses")[0]
+    if(i == 21):
+      iTLB_loads= dados_linha.split("iTLB-loads")[0]
+    if(i == 22):
+      iTLB_load_misses= dados_linha.split("iTLB-load-misses")[0]
+
     i= i+1
   arquivo_txt.close()
-#  arquivo_csv.close()
-#
-##coletar os dados dos arquivos e salvar em um arquivo principal -----------------
-#arq_resultado_geral= open(f'{raiz}/resultado_geral_perf.csv', 'w')
-#arq_resultado_geral.write("combined,algoritmo,packet_size,hook_ebpf,var_ip,timeout,rx_packets,rx_packet_rate_avg,rx_packet_rate\n")
+  arq_resultado_geral_perf.write(f"{task_clock},{context_switches},{cpu_migrations},{page_faults},{cycles},{instructions},{branches},{branch_misses},{L1_dcache_loads},{L1_dcache_load_misses},{LLC_loads},{LLC_load_misses},{L1_icache_load_misses},{dTLB_loads},{dTLB_load_misses},{iTLB_loads},{iTLB_load_misses}\n")
+arq_resultado_geral_perf.close()
+print(f"Todos os resultados do perf foram salvos no arquivo:\n\t'{pasta}/resultado_geral_perf.csv'\n")
 
+
+#3) analiza arquivos salvos pelo sar na maquina de execucao dos programas eBPF/XDP ------------------------
